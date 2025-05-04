34707ff26edb5853fd6c1a1679082d694d354390
Sai-kumaRRR <alamurisaikumar03@gmail.com>
2025-05-04 14:22:36 +0530

    04th May 2025

--- src/ex14_example/Lab59_example02.java
+++ src/ex14_example/Lab59_example02.java
@@ -1,4 +1,10 @@
 package ex14_example;
 
 public class Lab59_example02 {
+    public static void main(String[] args) {
+        System.out.println("python");
+
+        for(int i = 10; i<=5; i++);
+        System.out.println("i="+ 5);
+    }
 }
