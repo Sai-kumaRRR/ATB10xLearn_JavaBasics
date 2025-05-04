9c54ecc04ccf02c4fa93a51ba298d5dc3026c4f7
Sai-kumaRRR <alamurisaikumar03@gmail.com>
2025-05-04 12:42:52 +0530

    04th May 2025

---
+++ src/ex14_example/Lab58_example.java
@@ -0,0 +1,11 @@
+package ex14_example;
+
+public class Lab58_example {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+
+
+        for (int i = 0; i <= 5; i--) ;
+        System.out.println("i ,=>i");
+    }
+}
